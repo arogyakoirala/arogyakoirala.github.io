096921370d80a0464095d9cb274716fb5b144c87
Arogya Koirala <arogyakoirala@gmail.com>
2021-08-25 11:08:18 -0700

    Updated Bio

--- src/components/About.js
+++ src/components/About.js
@@ -14,11 +14,11 @@ function About({background, textColor}) {
     <div className="row " >
 
 
-    <div  className="col-md-7 about pt-5">
-               I am an information systems designer from Nepal. Currently, I am pursuing my graduate studies at the UC Berkeley School of Information. Additionally, I am also a Graduate Student Researcher at Cal, working as a Data Visualization Developer for the Graduate Division at UC Berkeley.  
+    <div  className="col-md-7 about pt-3">
+               I am an information systems designer from Nepal with 7+ years of experience in data science and application design and development (web and mobile). Currently, I am a <a href="https://www.ischool.berkeley.edu/programs/mims">graduate student</a> at the UC Berkeley School of Information. Additionally, I also work as Graduate Student Researcher, contributing my skills as a Data Visualization Developer for the Graduate Division.  
                <br/>
                <br/>
-               Before my move to the US, I worked as the Tech and Innovation Lead at <b><a href="http://kathmandulivinglabs.org">Kathmandu Living Labs</a></b>, where I designed and developed technology tools aimed at promoting and supporting the use of data in government, civil society, and the private sector. 
+               Prior to my move to the US, I worked as the Tech and Innovation Lead at <b><a href="http://kathmandulivinglabs.org">Kathmandu Living Labs</a></b>, where I designed and developed technology tools aimed at promoting and supporting the use of data in government, civil society, and the private sector. 
     </div>
 
     {/* <div  className="col-md-5 about pt-5">
