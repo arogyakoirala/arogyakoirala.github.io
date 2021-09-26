5623df2e1f1e2b6cd96d20d617649460523fb8f7
Arogya Koirala <arogya@berkeley.edu>
2021-09-26 08:22:47 -0700

    updated bio

--- src/components/About.js
+++ src/components/About.js
@@ -17,31 +17,30 @@ function About({ background, textColor }) {
 
       <div className="col-md-8 about pt-3"  >
         <span style={{ textTransform: 'uppercase', fontSize: '0.9rem', fontWeight: 700 }}>ABOUT</span >
-     
-        <br/>
-
-        <p className="m-0 mt-2 pb-0">I am an information professional currently pursuing the Master of Information Management and Systems (MIMS) program at UC Berkeley School of Information. I have seven years of experience in designing and developing data-driven, web and mobile based information systems for corporations, government agencies, and civil-society organizations in the US, India, and Nepal. My work so far has centered around the design and development of systems that generate (or ingest), analyze, and visualize complex data.</p>
 
         <br />
 
-        Prior to Berkeley, I was the Tech and Innovation Lead of <a href="https://kathmandulivinglabs.org">Kathmandu Living Labs</a> (KLL), where I led the design and development of numerous web and mobile development projects in areas such as disaster risk reduction, citizen science, good governance, and open data. In 2020, I led the design, development, and testing of Baato, Nepal’s first cloud based geospatial services company. Before KLL, I was a Decision Scientist for Mu Sigma Inc, where I worked on data analytics projects with numerous Fortune 500 companies in the banking, technology, and retail domains.
+        <p className="m-0 mt-2 pb-0">
+          I am an information professional currently pursuing the Master of Information Management and Systems (MIMS) program at UC Berkeley School of Information, where I'm focusing on gaining skills in research methods (qualitative and quantitative), machine learning, big data technologies, and data for development. Prior to coming to Berkeley, I served as the Tech and Innovation Lead for <a href="https://kathmandulivinglabs.org"> Kathmandu Living Labs.</a>
 
+        </p>
         <br />
-        <br />
 
-        My passion lies at the intersection of data, people, and systems. I am currently seeking summer internship opportunities in data science, user research, or product design. I prefer to work with companies that have a strong emphasis on learning, mentorship and teamwork.
 
-        <br/>
-        <br/>
+        <p>I'm interested in understanding how technology and data can support the disadvantaged. I have 7+ years of experience in designing and developing data-driven web and mobile applications for disaster risk reduction, good governance, civic engagement, and government transparency. I have a proven track record working across different information-related roles (tech lead, UX researcher, data scientist, full-stack developer, manager) with a wide variety of clients (government agencies, academia, fortune 500 companies, civil society organizations, youth groups) in different parts of the world (Nepal, India and the US).
 
-        <a class="resume-links" href="https://github.com/arogyakoirala/bucket/raw/master/AK_Resume_Sep212021.pdf" target="_blank" style={{fontSize: "1.1rem"}}>View Resume</a>
-       
+        </p>
+
+
+
+        <a class="resume-links" href="https://github.com/arogyakoirala/bucket/raw/master/AK_Resume_Sep212021.pdf" target="_blank" style={{ fontSize: "1.1rem" }}>View Resume</a>
+
       </div>
 
 
       {/* <div  className="col-md-4  about pt-5"> */}
-      <div className="hideInSmall" style={{position: "absolute", top:"0px", maxWidth:"300px", right: "15%"}}>
-     <img src="https://raw.githubusercontent.com/arogyakoirala/bucket/master/20210901_ISchoolPortraits_bhs_061.jpeg"  className="img-fluid" style={{borderBottomRightRadius: "20px"}}/>
+      <div className="hideInSmall" style={{ position: "absolute", top: "0px", maxWidth: "300px", right: "15%" }}>
+        <img src="https://raw.githubusercontent.com/arogyakoirala/bucket/master/20210901_ISchoolPortraits_bhs_061.jpeg" className="img-fluid" style={{ borderBottomRightRadius: "50px", borderBottomLeftRadius: "50px" }} />
       </div>
 
 
